78f0a91d608b8223b29a493707ac143cb3413522
Samidh Patel <spatel1345@gmail.com>
2019-04-17 15:17:46 -0400

    Add corrected seeds file.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_15_234714) do
+ActiveRecord::Schema.define(version: 2019_04_15_202307) do
 
   create_table "reviews", force: :cascade do |t|
     t.integer "student_tutor_id"
