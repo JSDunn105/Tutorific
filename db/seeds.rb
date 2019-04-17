8df94884872c07a12866110974e37c890d8fcd9d
@JSDunn105 <j.s.dunn@icloud.com>
2019-04-17 15:53:32 -0400

    Seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -29,29 +29,7 @@ art = Subject.create(name: "Art")
 bmath = Subject.create(name: "Basic Math")
 bio = Subject.create(name: "Biology")
 
-10.times do
-    User.create(
-         email: Faker::Internet.email,
-         first_name: Faker::Name.first_name,
-         last_name: Faker::Name.last_name,
-         profile: Faker::Lorem.sentences,
-     )
-end
-
-# Users (Tutors)
-jill = User.create(first_name: "Jill", last_name: "Valentine", profile: "I love to teach discrete math. It's one of the most amazing subjects")
-tom = User.create(first_name: "Tom", last_name: "Cruise", profile: "I was formely one of the greatest actors, but I found my passion in Chemistry")
-malik = User.create(first_name: "Malik", last_name: "Siddiqi", profile: "My name is Malik Siddiqi and I love teach history.")
-james = User.create(first_name: "James", last_name: "Bond", profile: "The worlds best spy, now teaching and mastering the art of artificial intelligence")
-kim = User.create(first_name: "Kim", last_name: "Bradford", profile: "I love teaching biology, since I have a PhD")
-john = User.create(first_name: "John", last_name: "Cena", profile: "You can't see me, but I teach physics")
-scott = User.create(first_name: "Scott", last_name: "Thompson", profile: "My favorite subject is calculus, since I'm a descendent of Newton")
-trump = User.create(first_name: "Donald", last_name: "Trump", profile: "I can't teach political science, because I'm into real estate. Ask me again, and you're fired!")
-kovacs = User.create(first_name: "Takeshi", last_name: "Kovacs", profile: "I'm from the feature, but I can teach biochemistry")
-ryan = User.create(first_name: "Ryan", last_name: "Reynolds", profile: "I'm in the new Pokemon movie, but I can teach you something about art")
- 
-
-5.times do
+ 5.times do
      User.create(
          email: Faker::Internet.email,
          first_name: Faker::Name.first_name,
@@ -71,6 +49,16 @@ ryan = User.create(first_name: "Ryan", last_name: "Reynolds", profile: "I'm in t
          profile: Faker::Lorem.sentences)
      end
 
-     TutorSubject.create(
-         tutor_id: User.id.first
+     TutorSubject.create(tutor_id: User.id.first, subject_id: ph.id)
+     TutorSubject.create(tutor_id: User.id.first, subject_id: ph.id)
+     TutorSubject.create(tutor_id: User.id.first, subject_id: ph.id)
+     TutorSubject.create(tutor_id: User.id.first, subject_id: ph.id)
+     TutorSubject.create(tutor_id: User.id.first, subject_id: ph.id)
+     TutorSubject.create(tutor_id: User.id.first, subject_id: ph.id)
+     TutorSubject.create(tutor_id: User.id.first, subject_id: ph.id)
+     TutorSubject.create(tutor_id: User.id.first, subject_id: ph.id)
+     TutorSubject.create(tutor_id: User.id.first, subject_id: ph.id)
+     TutorSubject.create(tutor_id: User.id.first, subject_id: ph.id)
+
+
      )
