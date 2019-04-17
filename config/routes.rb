7b87762096ff1ccf6c9f23f0737e5787583374a0
@JSDunn105 <j.s.dunn@icloud.com>
2019-04-17 15:45:46 -0400

    Route fixes and link updates

--- config/routes.rb
+++ config/routes.rb
@@ -5,11 +5,7 @@ Rails.application.routes.draw do
   resources :tutor_subjects
   resources :users
   resources :student_tutors
-<<<<<<< HEAD
-  resources :review
-=======
   resources :reviews
->>>>>>> eefa731... config routes update
 
   get '/users/subjects/:id', to: 'users#subjects'
   post '/users/subjects/:id', to: 'users#subjectupdate'
